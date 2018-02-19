ce45a1a93d324dc5641b4803af62a3473191da66
Koalapvh13 <matheuspvh13@gmail.com>
2018-02-19 10:53:53 -0400

    resolucao ate exercicio 5

---
+++ EstruturaSequencial/4_media.py
@@ -0,0 +1,8 @@
+#Faça um Programa que peça as 4 notas bimestrais e mostre a média
+
+n1 = float(input('1ª nota: '))
+n2 = float(input('2ª nota: '))
+n3 = float(input('3ª nota: '))
+n4 = float(input('4ª nota: '))
+m = (n1+n2+n3+n4)/4
+print('A sua média é {}.'.format(m))
---
+++ EstruturaSequencial/5_conversor_metros.py
@@ -0,0 +1,5 @@
+#Faça um Programa que converta metros para centímetros.
+
+metros = float(input('Informe os metros: '))
+centimetros = metros*100
+print('{} metros equivalem a {:.0f} centímetros.'.format(metros, centimetros))
