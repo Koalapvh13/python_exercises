a7a1484e04eb4175792222517e0459b5a3f4c042
Koalapvh13 <matheuspvh13@gmail.com>
2018-02-18 22:50:33 -0400

    primeiros 2 exercicios corrigido

--- EstruturaSequencial/2- Informar o número.py
+++ EstruturaSequencial/2- Informar o número.py
@@ -1,4 +1,4 @@
 #Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
 
 n = input('Informe um número: ')
-print('Voçê digitou o número {}!'.format(n))
+print('Você digitou o número {}!'.format(n))
